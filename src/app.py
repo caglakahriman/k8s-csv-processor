4531a246f7c60f7e06dd11ed4ba29690a27129f8
caglakahriman <nc.kahriman7@gmail.com>
2025-07-01 20:03:10 +0300

    create flask app to handle files, add static files to be served

---
+++ src/app.py
@@ -0,0 +1,45 @@
+import boto3
+import csv
+from flask import Flask, request, redirect, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('playground-test-processed-files')
+
+app = Flask(__name__, static_url_path='/static')
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1000 * 1000 # 10mb
+
+def allowed_file(filename):
+  return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['GET', 'POST'])
+def upload_file():
+  processed_files = [obj.key for obj in bucket.objects.all()]
+
+  if request.method == 'GET':
+    return render_template("index.html", files=processed_files)
+  
+  if request.method == 'POST':
+    if 'file' not in request.files:
+      return redirect("/")
+    
+    uploaded_file = request.files['file']
+
+    if uploaded_file.filename == '':
+      return redirect("/")
+    
+    if uploaded_file and allowed_file(uploaded_file.filename):
+      filename = secure_filename(uploaded_file.filename)
+      content = uploaded_file.stream.read().decode("utf-8").splitlines()
+      reader = csv.reader(content)
+      parsed_rows = list(reader)
+      uploaded_file.stream.seek(0)  # Reset stream for S3 upload
+      bucket.upload_fileobj(uploaded_file, filename)
+      processed_files = [obj.key for obj in bucket.objects.all()]
+      return render_template("index.html", rows=parsed_rows, files=processed_files)
+    
+    return render_template("index.html", files=processed_files)
+  
+  return render_template("index.html")
